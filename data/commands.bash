29de7c8753578249e2baefeb44f4594facd4ad32
Dave Dowding <ddowding@sparkgeo.com>
2024-06-20 14:13:24 +0100

    feat: updated types for industrial and real estate

---
+++ data/commands.bash
@@ -0,0 +1,24 @@
+toil-cwl-runner get_asset_impact.cwl#get-asset-impact-workflow --json_file https://raw.githubusercontent.com/geodowd/create_data/main/data/output_industrial_activity_10.json
+cp /Users/sgdowd/Documents/1_EODH/physrisk_cli/asset_output/results.geojson /Users/sgdowd/Documents/1_EODH/create_data/results/result_industrial_activity_10.json
+toil-cwl-runner get_asset_impact.cwl#get-asset-impact-workflow --json_file https://raw.githubusercontent.com/geodowd/create_data/main/data/output_industrial_activity_100.json
+cp /Users/sgdowd/Documents/1_EODH/physrisk_cli/asset_output/results.geojson /Users/sgdowd/Documents/1_EODH/create_data/results/result_industrial_activity_100.json
+toil-cwl-runner get_asset_impact.cwl#get-asset-impact-workflow --json_file https://raw.githubusercontent.com/geodowd/create_data/main/data/output_industrial_activity_1000.json
+cp /Users/sgdowd/Documents/1_EODH/physrisk_cli/asset_output/results.geojson /Users/sgdowd/Documents/1_EODH/create_data/results/result_industrial_activity_1000.json
+toil-cwl-runner get_asset_impact.cwl#get-asset-impact-workflow --json_file https://raw.githubusercontent.com/geodowd/create_data/main/data/output_industrial_activity_10000.json
+cp /Users/sgdowd/Documents/1_EODH/physrisk_cli/asset_output/results.geojson /Users/sgdowd/Documents/1_EODH/create_data/results/result_industrial_activity_10000.json
+toil-cwl-runner get_asset_impact.cwl#get-asset-impact-workflow --json_file https://raw.githubusercontent.com/geodowd/create_data/main/data/output_powergeneratingasset_10.json
+cp /Users/sgdowd/Documents/1_EODH/physrisk_cli/asset_output/results.geojson /Users/sgdowd/Documents/1_EODH/create_data/results/result_powergeneratingasset_10.json
+toil-cwl-runner get_asset_impact.cwl#get-asset-impact-workflow --json_file https://raw.githubusercontent.com/geodowd/create_data/main/data/output_powergeneratingasset_100.json
+cp /Users/sgdowd/Documents/1_EODH/physrisk_cli/asset_output/results.geojson /Users/sgdowd/Documents/1_EODH/create_data/results/result_powergeneratingasset_100.json
+toil-cwl-runner get_asset_impact.cwl#get-asset-impact-workflow --json_file https://raw.githubusercontent.com/geodowd/create_data/main/data/output_powergeneratingasset_1000.json
+cp /Users/sgdowd/Documents/1_EODH/physrisk_cli/asset_output/results.geojson /Users/sgdowd/Documents/1_EODH/create_data/results/result_powergeneratingasset_1000.json
+toil-cwl-runner get_asset_impact.cwl#get-asset-impact-workflow --json_file https://raw.githubusercontent.com/geodowd/create_data/main/data/output_powergeneratingasset_10000.json
+cp /Users/sgdowd/Documents/1_EODH/physrisk_cli/asset_output/results.geojson /Users/sgdowd/Documents/1_EODH/create_data/results/result_powergeneratingasset_10000.json
+toil-cwl-runner get_asset_impact.cwl#get-asset-impact-workflow --json_file https://raw.githubusercontent.com/geodowd/create_data/main/data/output_realestateasset_10.json
+cp /Users/sgdowd/Documents/1_EODH/physrisk_cli/asset_output/results.geojson /Users/sgdowd/Documents/1_EODH/create_data/results/result_realestateasset_10.json
+toil-cwl-runner get_asset_impact.cwl#get-asset-impact-workflow --json_file https://raw.githubusercontent.com/geodowd/create_data/main/data/output_realestateasset_100.json
+cp /Users/sgdowd/Documents/1_EODH/physrisk_cli/asset_output/results.geojson /Users/sgdowd/Documents/1_EODH/create_data/results/result_realestateasset_100.json
+toil-cwl-runner get_asset_impact.cwl#get-asset-impact-workflow --json_file https://raw.githubusercontent.com/geodowd/create_data/main/data/output_realestateasset_1000.json
+cp /Users/sgdowd/Documents/1_EODH/physrisk_cli/asset_output/results.geojson /Users/sgdowd/Documents/1_EODH/create_data/results/result_realestateasset_1000.json
+toil-cwl-runner get_asset_impact.cwl#get-asset-impact-workflow --json_file https://raw.githubusercontent.com/geodowd/create_data/main/data/output_realestateasset_10000.json
+cp /Users/sgdowd/Documents/1_EODH/physrisk_cli/asset_output/results.geojson /Users/sgdowd/Documents/1_EODH/create_data/results/result_realestateasset_10000.json
